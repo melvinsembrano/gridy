4362c059e3d3629b3b13d8e321028c54f21fad83
Melvin Sembrano <melv@hey.com>
2024-07-16 16:15:48 +1000

    add more tests

--- test/gridy/controller/actions_integration_test.rb
+++ test/gridy/controller/actions_integration_test.rb
@@ -27,4 +27,27 @@ class Gridy::Controller::ActionsIntegrationTest < ActionDispatch::IntegrationTes
     assert_redirected_to account_url(Account.last)
   end
 
+  test "should show account" do
+    get account_url(@account)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_account_url(@account)
+    assert_response :success
+  end
+
+  test "should update account" do
+    patch account_url(@account), params: { account: { email: @account.email, first_name: @account.first_name, last_name: @account.last_name } }
+    assert_redirected_to account_url(@account)
+  end
+
+  test "should destroy account" do
+    assert_difference('Account.count', -1) do
+      delete account_url(@account)
+    end
+
+    assert_redirected_to accounts_url
+  end
+
 end
